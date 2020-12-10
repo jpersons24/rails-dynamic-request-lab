f1c94dd5add171fe8d8f49cfd3988c86177b0b23
Jakob Persons <jakobep24@gmail.com>
2020-12-10 13:57:10 -0500

    show and index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+2.times do
+   Student.create(first_name: "Jake", last_name: "Persons")
+end
